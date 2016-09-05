09816d24a2569767ce888ea8d1840c61408fa4de
Lucas Nelson <artimage@ishiboo.com>
2016-09-05 18:54:38 -0400

    Add a translation for my new companies group.

--- packages/my-custom-package/lib/intl.js
+++ packages/my-custom-package/lib/intl.js
@@ -6,5 +6,6 @@ import Telescope from 'meteor/nova:lib';
 
 Telescope.strings.en = {
   ...Telescope.strings.en, // get all the string translated
-  "posts.color": "Color" // add a new one (collection.field: "Label")
+  "posts.color": "Color", // add a new one (collection.field: "Label")
+  "users.companies": "Companies" // add a new one (collection.field: "Label")
 };
